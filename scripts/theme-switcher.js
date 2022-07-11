70307f88a43ca48ef722dbd48f9c69bfe3e7601c
everythinguwannatobe <timdonerpower@gmail.com>
2022-07-10 22:46:17 -0400

    Modified files
    modified files

--- scripts/theme-switcher.js
+++ scripts/theme-switcher.js
@@ -1,42 +1,28 @@
-// Create a link element to fetch the dark-mode.css file
-let theme = document.createElement("link");
-theme.setAttribute("rel", "stylesheet");
-theme.setAttribute("type", "text/css");
+let themeLink = document.createElement("link");
+themeLink.setAttribute("rel", "stylesheet");
+themeLink.setAttribute("type", "text/css");
 const themeDirectory = './styles/themes/';
-let themeName = '';
+let themeSelectorOptions = document.getElementById("theme-selector");
 
-// Get the theme element and assign it to a variable
-let themeSelector = document.getElementById("theme-selector");
-
-//Load dark-mode.css file when the user selects the dark-mode option
-themeSelector.addEventListener("change", function () {
-    themeName = themeSelector.value;
-    theme.setAttribute("href", `${themeDirectory}${themeName}.css`);
-    if (themeSelector.value) {
-        document.getElementsByTagName("head")[0].appendChild(theme);
-    } else {
-        document.getElementsByTagName("head")[0].removeChild(theme);
-    }
-    // Save the user's choice to local storage
-    localStorage.setItem("theme", themeName);
-});
-
-
-const getThemeName = () => {
-
+export const switchTheme = () => {
+    themeSelectorOptions.addEventListener("change", function () {
+        const selectedThemeName = this.value;
+        let themePath = `${themeDirectory}${selectedThemeName}.css`;
+        themeLink.setAttribute("href", `${themePath}`);
+        document.getElementsByTagName("head")[0].appendChild(themeLink);
+        localStorage.setItem("theme", selectedThemeName);
+    });
 }
 
-window.onload = () => {
-    // Load the user's choice from local storage
-    themeName = localStorage.getItem("theme");
-    if (themeName) {
-        theme.setAttribute("href", `${themeDirectory}${themeName}.css`);
-        document.getElementsByTagName("head")[0].appendChild(theme);
-        console.log(`${themeName} theme is loaded from local storage`);
+export const reloadTheme = () => {
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+        themeSelectorOptions.value = theme;
+        themeLink.setAttribute("href", `${themeDirectory}${theme}.css`);
+        document.getElementsByTagName("head")[0].appendChild(themeLink);
     } else {
-        // else load the first option as the default theme
-        theme.setAttribute("href", `${themeDirectory}${themeSelector.value}.css`);
-        document.getElementsByTagName("head")[0].appendChild(theme);
-        console.log(`${themeSelector.value} theme is set as default`);
+        const defaultTheme = themeSelectorOptions.value;
+        themeLink.setAttribute("href", `${themeDirectory}${defaultTheme}.css`);
+        document.getElementsByTagName("head")[0].appendChild(themeLink);
     }
-};
+}
